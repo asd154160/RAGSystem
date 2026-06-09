0c3e30db5e05620d46c928cbf7d5a2a1adecc6c2
asd154160 <aa1541602596@gmail.com>
2026-06-09 09:38:00 +0800

    feat: UI 组件统一导出 barrel

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./button";
+export { Input, Textarea } from "./input";
+export { Badge } from "./badge";
+export { Avatar } from "./avatar";
+export { Card } from "./card";
+export { Modal } from "./modal";
+export { EmptyState } from "./empty-state";
+export { ToastProvider, useToast } from "./toast";
